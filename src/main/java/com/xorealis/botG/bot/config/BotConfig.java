de9c62d842dc652115cfd7959d89ef8b129f8cd9
flowesdown <showerpool@gmail.com>
2025-01-24 21:43:55 +0300

    add BotConfig

--- src/main/java/com/xorealis/botG/bot/config/BotConfig.java
+++ src/main/java/com/xorealis/botG/bot/config/BotConfig.java
@@ -1,9 +1,18 @@
 package com.xorealis.botG.bot.config;
 
 
-import org.springframework.stereotype.Component;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@Component
+@Configuration
+@Data
+@PropertySource("application.properties")
 public class BotConfig {
+    @Value("${bot.name}")
+    String botName;
 
+    @Value("${bot.token}")
+    String token;
 }
---
+++ src/main/java/com/xorealis/botG/bot/config/BotInitializer.java
@@ -0,0 +1,14 @@
+package com.xorealis.botG.bot.config;
+
+
+import lombok.Data;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+public class BotInitializer {
+
+
+}
